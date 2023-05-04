26ebedc0fefd3421241e3837b47c8b58022a18e5
JSR5404 <joshrice5404@gmail.com>
2023-05-03 23:02:18 -0400

    user model creation

---
+++ models/users.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require('mongoose');
+
+const usersSchema = new Schema(
+    {
+        username:
+        {
+            type: String,
+            required: true,
+            unique: true
+        },
+     
+    
+        email:
+        {
+            type: String,
+            required: true,
+            unique: true
+        },
+    
+    thoughts: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Thoughts'
+      }],
+
+    friends: [{type: Schema.Types.ObjectId,
+        ref: 'Users'
+    
+    }] 
+
+},
+{
+    toJSON: {
+      virtuals: true,
+      getters: true
+      
+    },
+    id: false,
+  }
+);
+
+const User = model('Users', usersSchema);
+
+module.exports = User;
