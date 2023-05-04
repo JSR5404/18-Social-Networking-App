920eb63f342550ad0957ae083161b83bad385a27
JSR5404 <joshrice5404@gmail.com>
2023-05-04 04:19:15 -0400

    user controller work

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -2,7 +2,13 @@ const { Users, Thoughts } = require("../models");
 
 const usersController = {
 
-
+    getAllUsers(req, res) { Users.find({}).then((data) => res.json(data))},
+    getUser(req, res) {Users.findOne({ _id: params.id }).then((data) => res.json(data)) },
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend
 
 };
 
